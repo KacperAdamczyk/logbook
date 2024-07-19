bcd0e90831d4b4bb23605f0291b253b6f70f4193
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2024-07-19 19:46:36 +0200

    Add authjs

---
+++ drizzle.config.ts
@@ -0,0 +1,11 @@
+import { env } from "@/env";
+import { defineConfig } from "drizzle-kit";
+export default defineConfig({
+	schema: "./db/schema.ts",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: env.DATABASE_URL,
+	},
+	verbose: true,
+	strict: true,
+});
