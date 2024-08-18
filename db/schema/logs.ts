fa44276248342c890fbbd79944e4b820460f8b05
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2024-08-18 23:53:46 +0200

    Add defaults

--- db/schema/logs.ts
+++ db/schema/logs.ts
@@ -25,20 +25,31 @@ export const logs = sqliteTable("logs", {
 	pilotInCommandId: text("pilot_in_command_id")
 		.notNull()
 		.references(() => pilots.id),
-	singlePilotTimeSingleEngine: integer("single_pilot_time_single_engine"),
-	singlePilotTimeMultiEngine: integer("single_pilot_time_multi_engine"),
-	multiPilotTime: integer("multi_pilot_time"),
-	totalFlightTime: integer("total_flight_time"),
-	takeoffsDay: integer("takeoffs_day"),
-	takeoffsNight: integer("takeoffs_night"),
-	landingsDay: integer("landings_day"),
-	landingsNight: integer("landings_night"),
-	operationalConditionTimeNight: integer("operational_condition_time_night"),
-	operationalConditionTimeIfr: integer("operational_condition_time_ifr"),
-	functionTimePilotInCommand: integer("function_time_pilot_in_command"),
-	functionTimeCoPilot: integer("function_time_co_pilot"),
-	functionTimeDual: integer("function_time_dual"),
-	functionTimeInstructor: integer("function_time_instructor"),
+	singlePilotTimeSingleEngine: integer(
+		"single_pilot_time_single_engine",
+	).default(0),
+	singlePilotTimeMultiEngine: integer("single_pilot_time_multi_engine").default(
+		0,
+	),
+	multiPilotTime: integer("multi_pilot_time").default(0),
+	totalFlightTime: integer("total_flight_time").default(0),
+	takeoffsDay: integer("takeoffs_day").default(0),
+	takeoffsNight: integer("takeoffs_night").default(0),
+	landingsDay: integer("landings_day").default(0),
+	landingsNight: integer("landings_night").default(0),
+	operationalConditionTimeNight: integer(
+		"operational_condition_time_night",
+	).default(0),
+	operationalConditionTimeIfr: integer(
+		"operational_condition_time_ifr",
+	).default(0),
+	functionTimePilotInCommand: integer("function_time_pilot_in_command").default(
+		0,
+	),
+	functionTimeCoPilot: integer("function_time_co_pilot").default(0),
+	functionTimeDual: integer("function_time_dual").default(0),
+	functionTimeInstructor: integer("function_time_instructor").default(0),
+	remarks: text("remarks"),
 });
 
 export const logsRelations = relations(logs, ({ one }) => ({
