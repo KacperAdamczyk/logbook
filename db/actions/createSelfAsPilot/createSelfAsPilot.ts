9c37db633dee68f9b0b357527b4adfc01234df2f
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2024-09-02 12:51:49 +0200

    Rename default pilot entry to "self"

--- db/actions/createSelfAsPilot/createSelfAsPilot.ts
+++ db/actions/createSelfAsPilot/createSelfAsPilot.ts
@@ -2,30 +2,26 @@ import { type Pilot, pilots } from "@/db/schema";
 import { createDbAction } from "@/db/utils";
 
 export interface CreateSelfAsPilotArgs {
-	id: string;
+	userId: string;
 }
 
+const DEFAULT_PILOT_NAME = "Self";
+
 export const createSelfAsPilot = createDbAction<CreateSelfAsPilotArgs, Pilot>(
-	async (tx, { id }) => {
+	async (tx, { userId }) => {
 		const user = await tx.query.users.findFirst({
-			where: (users, { eq }) => eq(users.id, id),
+			where: (users, { eq }) => eq(users.id, userId),
 		});
 
 		if (!user) {
 			throw new Error("User not found");
 		}
 
-		const { name } = user;
-
-		if (!name) {
-			throw new Error("User does not have a name");
-		}
-
 		const [newPilot] = await tx
 			.insert(pilots)
 			.values({
-				userId: id,
-				name,
+				userId,
+				name: DEFAULT_PILOT_NAME,
 			})
 			.returning();
 
