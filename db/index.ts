0ef00f5703eee94338ed9eaebf0893a3123f1a08
Kacper Adamczyk <kacper.k.adamczyk@gmail.com>
2024-10-08 19:37:20 +0200

    Rollback drizzle connect

--- db/index.ts
+++ db/index.ts
@@ -1,14 +1,22 @@
 import { env } from "@/env";
-import { drizzle } from "drizzle-orm/connect";
+import { drizzle } from "drizzle-orm/libsql";
+import { createClient } from "@libsql/client";
 
 import * as schema from "./schema";
 
-export const db = await drizzle("turso", {
-	connection: {
-		url: env.DATABASE_URL,
-		authToken: env.DATABASE_TOKEN,
-	},
-	schema,
-	casing: "snake_case",
+const sql = createClient({
+	url: env.DATABASE_URL,
+	authToken: env.DATABASE_TOKEN,
 });
+
+export const db = drizzle(sql, { schema });
+
+// export const db = await drizzle("turso", {
+// 	connection: {
+// 		url: env.DATABASE_URL,
+// 		authToken: env.DATABASE_TOKEN,
+// 	},
+// 	schema,
+// 	casing: "snake_case",
+// });
 export type Tx = Parameters<Parameters<typeof db.transaction>[0]>[0];
