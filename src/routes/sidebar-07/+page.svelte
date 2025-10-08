2143a224c00518e731123b1a70b5007f7ffd0700
Kacper Adamczyk <kacper@admck.com>
2025-10-08 18:12:14 +0200

    Add sidenav layout

---
+++ src/routes/sidebar-07/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
+	import {
+		Breadcrumb,
+		BreadcrumbList,
+		BreadcrumbItem,
+		BreadcrumbLink,
+		BreadcrumbSeparator,
+		BreadcrumbPage
+	} from '$lib/components/ui/breadcrumb/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import { SidebarProvider, SidebarInset, SidebarTrigger } from '$lib/components/ui/sidebar/index.js';
+</script>
+
+<SidebarProvider>
+	<AppSidebar />
+	<SidebarInset>
+		<header
+			class="group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear"
+		>
+			<div class="flex items-center gap-2 px-4">
+				<SidebarTrigger class="-ml-1" />
+				<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
+				<Breadcrumb>
+					<BreadcrumbList>
+						<BreadcrumbItem class="hidden md:block">
+							<BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
+						</BreadcrumbItem>
+						<BreadcrumbSeparator class="hidden md:block" />
+						<BreadcrumbItem>
+							<BreadcrumbPage>Data Fetching</BreadcrumbPage>
+						</BreadcrumbItem>
+					</BreadcrumbList>
+				</Breadcrumb>
+			</div>
+		</header>
+		<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+			<div class="grid auto-rows-min gap-4 md:grid-cols-3">
+				<div class="bg-muted/50 aspect-video rounded-xl"></div>
+				<div class="bg-muted/50 aspect-video rounded-xl"></div>
+				<div class="bg-muted/50 aspect-video rounded-xl"></div>
+			</div>
+			<div class="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min"></div>
+		</div>
+	</SidebarInset>
+</SidebarProvider>
