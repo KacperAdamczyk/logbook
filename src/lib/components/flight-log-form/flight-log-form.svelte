e86a8f6e68d24b64c6007ca8c91e5d65c97045ec
Kacper Adamczyk <kacper@admck.com>
2025-11-10 16:20:14 +0100

    Add calendar components and flight log schema with form handling

---
+++ src/lib/components/flight-log-form/flight-log-form.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { RemoteForm } from '@sveltejs/kit';
+
+	import type { FlightLogSchema } from '$lib/remotes/flight-log/flight-log.schema';
+
+	interface Props {
+		remote: RemoteForm<FlightLogSchema, void>;
+	}
+
+	const { remote }: Props = $props();
+	let f = $state<string>();
+</script>
+
+<form {...remote}></form>
--- src/lib/remotes/create-flight-log/create-flight-log.remote.ts
+++
@@ -1,6 +0,0 @@
-import { form } from '$app/server';
-import { createFlightLogSchema } from '$lib/remotes/create-flight-log/create-flight-log.schema';
-
-export const createFlightLog = form(createFlightLogSchema, async (data) => {
-	console.log('data', data);
-});
