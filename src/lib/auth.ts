b08cf2b6b56bb516610f30b393521d7fad8833c9
Kacper Adamczyk <kacper@admck.com>
2026-03-13 15:46:16 +0100

    Adjust config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,18 +3,8 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "$lib/server/db";
 import { sveltekitCookies } from "better-auth/svelte-kit";
 import { getRequestEvent } from "$app/server";
-import { env } from "$env/dynamic/private";
-
-if (!env.TRUSTED_ORIGINS) {
-	throw new Error("Missing TRUSTED_ORIGINS environment variable");
-}
-
-const trustedOrigins = env.TRUSTED_ORIGINS.split(",")
-	.map((origin) => origin.trim())
-	.filter(Boolean);
 
 export const auth = betterAuth({
-	trustedOrigins,
 	database: drizzleAdapter(db, {
 		provider: "sqlite",
 	}),
@@ -35,6 +25,7 @@ export const auth = betterAuth({
 		sendOnSignUp: true,
 		sendOnSignIn: true,
 	},
+	trustedOrigins: ["http://localhost:5173", "https://*.-kacper-adamczyk-projects.vercel.app"],
 	plugins: [sveltekitCookies(getRequestEvent)],
 });
 
